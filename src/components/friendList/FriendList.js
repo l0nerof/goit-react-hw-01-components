fead70fe6de38962d74896b79ce8dc66f9962fa8
Volodymyr Bondar <2005volka@gmail.com>
2023-05-24 09:32:59 +0300

    redone

--- src/components/friendList/FriendList.js
+++ src/components/friendList/FriendList.js
@@ -1,16 +1,11 @@
 import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
+import FriendListItem from './FriendListItem';
 
 function FriendList({ friends }) {
   return friends.map(({ avatar, name, isOnline, id }) => (
     <ul className={css.FriendList} key={id}>
-      <li className={css.item}>
-        <span className={isOnline ? css.statusTrue : css.statusFalse}>
-          {isOnline}
-        </span>
-        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-        <p className={css.name}>{name}</p>
-      </li>
+      <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
     </ul>
   ));
 }
